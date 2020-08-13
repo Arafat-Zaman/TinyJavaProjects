2b41b483c14bfc0752856936210999a4dd4f6280
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2020-08-13 20:57:11 +0600

    Netbeans integration with java projects

---
+++ src/digitalclock/ClockThread.java
@@ -0,0 +1,22 @@
+package digitalclock;
+import java.util.Date;
+/**
+ *
+ * @author Arafat-Zaman
+ */
+public class ClockThread extends Thread{
+    DigitalClock dc;
+    String time;
+    
+    public ClockThread(DigitalClock dc){
+        this.dc = dc;
+        start();
+    }
+    
+    public void run(){
+        while(true){
+            time = ""+new Date();
+            dc.jlabClock.setText(time);
+        }
+    }
+}
